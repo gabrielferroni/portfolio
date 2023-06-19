f6888896511c22e1f5b12fed0366933fc343f48b
Gabriel Ferroni <112531131+gabrielferroni@users.noreply.github.com>
2023-06-19 17:56:46 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
-//alert("Olá, seja Bem-vindo(a). Servidor ainda encontra-se em manutenção para apresentação dos projetos e serviços!")
+alert("Olá, seja Bem-vindo(a). Servidor ainda encontra-se em manutenção para apresentação dos projetos e serviços!");
 
+
+//navbar...
 function clickMenu() {
     if(navbar.style.display == 'block') {
-        navbar.style.display = 'none'
+        navbar.style.display = 'none';
     } else {
-        navbar.style.display = 'block'
+        navbar.style.display = 'block';
     }
-}
+};
