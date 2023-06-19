d4273aa2acfeffe4451dd2bb3ea1967aa016c190
Gabriel Ferroni <112531131+gabrielferroni@users.noreply.github.com>
2023-06-19 17:51:49 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+//alert("Olá, seja Bem-vindo(a). Servidor ainda encontra-se em manutenção para apresentação dos projetos e serviços!")
+
+function clickMenu() {
+    if(navbar.style.display == 'block') {
+        navbar.style.display = 'none'
+    } else {
+        navbar.style.display = 'block'
+    }
+}
